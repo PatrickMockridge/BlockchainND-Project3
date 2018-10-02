460924bdb145f111e58009d65610e9b3886d85f3
Patrick Mockridge <patrick@engzig.com>
2018-10-02 17:36:26 +0100

    .gitignore now working

--- dist/blockchain/index.js
+++
@@ -1,8 +0,0 @@
-// lib/things/index.js
-// import Block from './block';
-// import Blockchain from './blockchain';
-
-// export { Block, Blockchain }
-
-export { default as Block } from './block.js';
-export { default as Blockchain } from './blockchain.js';
--- dist/controllers/index.js
+++
@@ -1,9 +0,0 @@
-// lib/things/index.js
-import { chain, info, block, getBlock } from './blockChainController';
-
-export default {
-  chain,
-  info,
-  block,
-  getBlock
-};
